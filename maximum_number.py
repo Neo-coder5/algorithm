bfb1aa7d4a8a9880894fc887eb6df0c026f461fd
Elmurod <elmurodmarimov7655@gmail.com>
2026-09-03 14:42:45 +0500

    add factorial

--- maximum_number.py
+++ maximum_number.py
@@ -1,15 +1,15 @@
-# def maximum_number(a,b,c):
-#     if a>b:
-#         if a>c:
-#             return a
-#         else:
-#             return c
-#     else:
-#         if b>c:
-#             return b
-#         else:
-#             return c
-# print(maximum_number(3,7,11))
+def maximum_number(a,b,c):
+    if a>b:
+        if a>c:
+            return a
+        else:
+            return c
+    else:
+        if b>c:
+            return b
+        else:
+            return c
+print(maximum_number(3,7,11))
 
 list1 = [4, 12, 5, 22, 8, 15, 3]
 class Solution:
@@ -26,3 +26,14 @@ class Solution:
         return max_num
 sol = Solution()
 print(sol.max(list1))
+
+class Solution:
+    def Factorial(self, n):
+        i = 1
+        k = 1
+        while i != n+1:
+            k *= i
+            i += 1
+        return k
+sol = Solution()
+print(sol.Factorial(5))
