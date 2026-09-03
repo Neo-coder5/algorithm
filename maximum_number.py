a975560ba1cf2079d6924648905e080aad0df41a
Elmurod <elmurodmarimov7655@gmail.com>
2026-09-03 14:28:20 +0500

    add find max of numbers with while

--- maximum_number.py
+++ maximum_number.py
@@ -1,12 +1,28 @@
-def maximum_number(a,b,c):
-    if a>b:
-        if a>c:
-            return a
-        else:
-            return c
-    else:
-        if b>c:
-            return b
-        else:
-            return c
-print(maximum_number(3,7,11))
+# def maximum_number(a,b,c):
+#     if a>b:
+#         if a>c:
+#             return a
+#         else:
+#             return c
+#     else:
+#         if b>c:
+#             return b
+#         else:
+#             return c
+# print(maximum_number(3,7,11))
+
+list1 = [4, 12, 5, 22, 8, 15, 3]
+class Solution:
+    def max(self, list1):
+        if not list1:
+            return None
+        max_num = list1[0]
+
+        i = 1
+        while i < len(list1):
+            if list1[i] > max_num:
+                max_num = list1[i]
+            i += 1
+        return max_num
+sol = Solution()
+print(sol.max(list1))
