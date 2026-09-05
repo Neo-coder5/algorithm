710e577f403a543a0c3e76a4fb55d54958ac4377
Elmurod <elmurodmarimov7655@gmail.com>
2026-09-05 15:11:33 +0500

    add binary search

--- searching_algorithms.py
+++ searching_algorithms.py
@@ -13,19 +13,39 @@
 #
 #
 
-myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
-class LinearSearch:
-    def find_number(self, list1, target):
-        for i in range(len(list1)):
-            ans = input(f"thinkung number: {n}? (y/n) ")
 
-            if list1[i] == target:
-                return list1[i]
+# myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
+# class LinearSearch2:
+#     def find_number(self, list1, target):
+#         for i in range(len(list1)):
+#             ans = input(f"thinkung number: {n}? (y/n) ")
+#
+#             if list1[i] == target:
+#                 return list1[i]
+#
+#
+#         return None
+#
+# sol = LinearSearch2()
+# print(sol.find_number(myList, 7))
 
-
+class BinarySearch:
+    def binary_search_f(self, lists, item):
+        low = 0
+        high = len(lists) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            guess = lists[mid]
+            if guess == item:
+                return mid
+            if guess > item:
+                high = mid - 1
+            else:
+                low = mid + 1
         return None
+list1 = [1,3,4,6,7,8,10,12,23,45,56,78,99]
+sol = BinarySearch()
+print(sol.binary_search_f(list1, 7))
 
-sol = LinearSearch()
-print(sol.find_number(myList, 7))
 
 
