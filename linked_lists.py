9c7fa27cec3c32987917f4f2dbc30730e68a7ebb
Elmurod <elmurodmarimov7655@gmail.com>
2026-09-06 23:21:19 +0500

    add new data to head of list

--- linked_lists.py
+++ linked_lists.py
@@ -12,3 +12,8 @@ class LinkedList:
         while temp:
             print(temp.data)
             temp = temp.next
+
+    def push(self,new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
