b5cbefd26ebaee323a4110b4802655c412ce20f5
Elmurod <elmurodmarimov7655@gmail.com>
2026-09-07 18:29:17 +0500

    add append to list

--- linked_lists.py
+++ linked_lists.py
@@ -25,3 +25,13 @@ class LinkedList:
         new_node = Node(new_data)
         new_node.next = prev_node.next
         prev_node.next = new_node
+
+    def append(self, new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
